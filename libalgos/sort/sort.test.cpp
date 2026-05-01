87fe218d837217a5852ef3a0e05ba058ff050c53
Senya <seni190999@mail.ru>
2026-05-01 15:22:47 +0500

    test(selection_sort): add more tests

--- libalgos/sort/sort.test.cpp
+++ libalgos/sort/sort.test.cpp
@@ -8,3 +8,27 @@ TEST(SelectionSortTest, NormalCollection){
     algos::sort::selection_sort(sample.begin(), sample.end(), [](auto lhs, auto rhs) { return lhs < rhs;});
     EXPECT_EQ(std::vector<int>({1, 1, 2, 5}), sample);
 }
+
+TEST(SelectionSortTest, EmptyCollection){
+    std::vector<int> sample = {};
+    algos::sort::selection_sort(sample.begin(), sample.end(), [](auto lhs, auto rhs) { return lhs < rhs;});
+    EXPECT_EQ(std::vector<int>({}), sample);
+}
+
+TEST(SelectionSortTest, AlreadySortedCollection){
+    std::vector<int> sample = {1, 2, 3, 4};
+    algos::sort::selection_sort(sample.begin(), sample.end(), [](auto lhs, auto rhs) { return lhs < rhs;});
+    EXPECT_EQ(std::vector<int>({1, 2, 3, 4}), sample);
+}
+
+TEST(SelectionSortTest, OneElementCollection){
+    std::vector<int> sample = {1};
+    algos::sort::selection_sort(sample.begin(), sample.end(), [](auto lhs, auto rhs) { return lhs < rhs;});
+    EXPECT_EQ(std::vector<int>({1}), sample);
+}
+
+TEST(SelectionSortTest, ReverseOrderSortedCollection) {
+    std::vector<int> sample = {4, 3, 2, 1};
+    algos::sort::selection_sort(sample.begin(), sample.end(), [](auto lhs, auto rhs) { return lhs < rhs;});
+    EXPECT_EQ(std::vector<int>({1, 2, 3, 4}), sample);
+}
