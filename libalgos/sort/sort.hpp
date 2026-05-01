9fc41755e9c8f89effd53842bcd114eb4bb6379b
Senya <seni190999@mail.ru>
2026-05-01 15:23:34 +0500

    feat(selection sort): add check for an empty collection

--- libalgos/sort/sort.hpp
+++ libalgos/sort/sort.hpp
@@ -3,6 +3,8 @@
 namespace algos::sort {
     template <typename RandomItType, typename CompType>
     void selection_sort(RandomItType begin, RandomItType end, CompType comp){
+        // Check for an emty collection
+        if (begin == end) return;
         for (auto it = begin; it < end - 1; ++it){
             auto selected = std::min_element(it, end, comp);
             std::iter_swap(selected, it);
